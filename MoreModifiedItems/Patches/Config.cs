93e6c80785038627ca56b27699a8e685f131cea6
M3dicCookie <pred102@hotmail.com>
2023-07-01 20:42:27 -0700

    Update Config.cs

--- MoreModifiedItems/Patches/Config.cs
+++ MoreModifiedItems/Patches/Config.cs
@@ -1,15 +1,1 @@
-﻿using System;
-using Nautilus.Json;
-using Nautilus.Options.Attributes;
-
-namespace AlexejheroYTB.MoreModifiedItems.Patches
-{
-	// Token: 0x0200000A RID: 10
-	[Menu("MoreModifiedItems")]
-	public class Config : ConfigFile
-	{
-		// Token: 0x0400001B RID: 27
-		[Slider("currentWreckSpeedMultiplier ", 0.1f, 10f, DefaultValue = 1f, Format = "{0:R0}")]
-		public float currentWreckSpeedMultiplier = 0f;
-	}
-}
+﻿
