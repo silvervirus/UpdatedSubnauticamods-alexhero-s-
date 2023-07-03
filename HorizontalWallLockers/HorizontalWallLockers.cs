0129fc278674115d8f23efff90cf989ebf6fe1a3
M3dicCookie <pred102@hotmail.com>
2023-07-03 01:02:01 -0700

    Add files via upload

---
+++ HorizontalWallLockers/HorizontalWallLockers.cs
@@ -0,0 +1,74 @@
+﻿using Nautilus.Assets;
+using Nautilus.Assets.Gadgets;
+using Nautilus.Assets.PrefabTemplates;
+using Nautilus.Crafting;
+using Nautilus.Handlers;
+using RamuneLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using static CraftData;
+
+namespace AlexejheroYTB.HorizontalWallLockers.prefab
+{
+   
+    internal class HorizontalWallLockers 
+    {
+        public static Texture2D HorizontalWallLockersTexture = Utilities.GetTexture("submarine_locker_01");
+        public static Texture2D HorizontalWallLockersspec = Utilities.GetTexture("submarine_locker_01_spec");
+        public static Texture2D HorizontalWallLockersnorm = Utilities.GetTexture("submarine_locker_01_normal");
+        public static PrefabInfo Info;
+        public static TechType TechType;
+
+        public static void Patch()
+
+        {
+
+
+            Info = Utilities.CreatePrefabInfo("horizontalwalllocker", "Horizontal Wall Locker", "Small, wall-mounted storage solution.", Utilities.GetSprite("locker"), 2, 3);
+            var prefab = new CustomPrefab(Info);
+
+            
+        prefab.SetUnlock(TechType.Peeper);
+            prefab.SetPdaGroupCategory(TechGroup.InteriorModules, TechCategory.InteriorModule);
+            prefab.SetRecipe(new RecipeData(new CraftData.Ingredient(TechType.Titanium, 1)));
+
+
+
+
+            prefab.SetGameObject(new CloneTemplate(Info, TechType.SmallLocker)
+            {
+                ModifyPrefab = prefab1 =>
+                {
+
+                    var renderer = prefab1.FindChild("model").FindChild("submarine_locker_02").FindChild("submarine_locker_02_door").GetComponent<MeshRenderer>();
+                    foreach (var m in renderer.materials)
+                    {
+                        m.mainTexture = HorizontalWallLockersTexture;
+                        m.SetTexture("_SpecTex", HorizontalWallLockersTexture);
+                        m.SetTexture("_Illum", HorizontalWallLockersTexture);
+                        m.SetTexture("_normal", HorizontalWallLockersnorm);
+                        m.SetColor("_Color", new Color(0.0f, 0.0f, 0.0f, 1.0f));
+                    }
+
+                    prefab1.FindChild("model").transform.rotation = Quaternion.Euler(0, 0, 90);
+                    
+
+                }
+                
+        });
+
+
+
+            prefab.Register();
+
+            TechType = Info.TechType;
+
+
+        }
+        
+    }
+}
+
---
+++ HorizontalWallLockers/Mod.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using Logger = AlexejheroYTB.Common.Logger;
+namespace AlexejheroYTB.HorizontalWallLockers.main
+{
+    [BepInPlugin(QMod.PLUGIN_GUID, QMod.PLUGIN_NAME, QMod.PLUGIN_VERSION)]
+    public  class QMod : BaseUnityPlugin
+    {
+        public const string PLUGIN_GUID = "SN.HorizontalWallLockers";
+
+        // Token: 0x04000007 RID: 7
+        public const string PLUGIN_NAME = "SN.HorizontalWallLockers";
+
+        // Token: 0x04000008 RID: 8
+        public const string PLUGIN_VERSION = "1.0.0";
+        public void Awake()
+        {
+            prefab.HorizontalWallLockers.Patch();
+
+            Logger.LogInfo("Patched");
+        }
+    }
+
+   
+    
+}
